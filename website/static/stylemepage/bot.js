5a191405c62edf697c59f474170931a925868947
Shubham Mishra <smdell83@gmail.com>
2023-08-09 22:51:18 +0530

    Added app code

--- website/static/stylemepage/bot.js
+++ website/static/stylemepage/bot.js
@@ -1,25 +1,35 @@
-const chatBox = document.getElementById('chat-box');
-const userInput = document.getElementById('user-input');
-const sendButton = document.getElementById('send-button');
-
-sendButton.addEventListener('click', sendMessage);
-
-function sendMessage() {
-    const message = userInput.value;
-    
-    if (message.trim() !== '') {
-        displayMessage('user', message);
-        // Replace this with your chatbot's response logic
-        setTimeout(() => displayMessage('bot', 'Hi,I am your Fashion Advisor, How may I assist you?'), 1500);
-        
-        userInput.value = '';
-    }
-}
-
-function displayMessage(sender, text) {
-    const messageDiv = document.createElement('div');
-    messageDiv.classList.add(sender);
-    messageDiv.textContent = text;
-    chatBox.appendChild(messageDiv);
+// JavaScript code for adding messages
+function addBotMessage(message) {
+    const chatBox = document.getElementById('chat-box');
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    messageElement.classList.add('message', 'bot-message');
+    chatBox.appendChild(messageElement);
+    // Scroll chat box to the bottom
     chatBox.scrollTop = chatBox.scrollHeight;
 }
+
+function addCustomerMessage(message) {
+    const chatBox = document.getElementById('chat-box');
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    messageElement.classList.add('message', 'customer-message');
+    chatBox.appendChild(messageElement);
+    // Clear the input field
+    document.getElementById('user-input').value = '';
+    // Scroll chat box to the bottom
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+// Event listener for the send button
+document.getElementById('send-button').addEventListener('click', function() {
+    const userInput = document.getElementById('user-input').value;
+    if (userInput) {
+        addCustomerMessage(userInput);
+        // Here, you would typically call a function to process the user's input and get a bot response
+        // For now, I'll simulate a bot response after a short delay
+        setTimeout(function() {
+            addBotMessage("Here's a stylish dress recommendation for you!");
+        }, 1000);
+    }
+});
