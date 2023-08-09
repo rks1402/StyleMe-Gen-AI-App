78bade8580a37f4ccd8f46d8448a1a2c8e2570db
Rahul Kumar Sharma <rahulkumarsharma@Rahuls-MacBook-Air.local>
2023-08-09 13:35:34 +0530

    modified code to be more modern and functional.

--- APIs/getproductByOcassion/main.py
+++ APIs/getproductByOcassion/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from google.cloud import datastore
 from google.oauth2 import service_account
 
@@ -11,11 +11,18 @@ client = datastore.Client(credentials=credentials)
 
 @app.route('/products', methods=['GET'])
 def get_products():
+    # Get the occasion parameter from the request arguments
     ocassion = request.args.get('ocassion')
+    if not ocassion:
+        return jsonify({'error': 'Missing occasion parameter'}), 400
+
+    # Query the GCP Datastore for products with the given occasion
     query = client.query(kind='MasterData')
     query.add_filter('ocassion', '=', ocassion)
     results = list(query.fetch())
-    return {'products': results}
+
+    # Return the results as a JSON object
+    return jsonify({'products': results})
 
 if __name__ == '__main__':
     app.run()
