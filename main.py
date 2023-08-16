43124eb5813709e693bc6eecb26125925f2880ec
Shubham Mishra <smdell83@gmail.com>
2023-08-16 18:29:28 +0530

    Signup page integration

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 from website import create_app
 
 # Get the directory of the current script
-dir_path = os.path.dirname(os.path.abspath(__file__))
+#dir_path = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the absolute path of the credentials file in the secrets directory
-credentials_path = os.path.join(dir_path, 'secrets', 'credentials.json')
+#credentials_path = os.path.join(dir_path, 'secrets', 'credentials.json')
 
 # Set the environment variable to the credentials file path
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
 # Create and run the Flask app
 app = create_app()
