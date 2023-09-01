1d2e0aac47886383ef5f0fdee2b8b1d1b41e687d
ankur verma <verma.ankur1729@gmail.com>
2023-09-01 17:02:34 +0530

    shopnowbutton

--- website/static/marketingpage/scripts.js
+++ website/static/marketingpage/scripts.js
@@ -1,73 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const chatArea = document.getElementById('chat-area');
-    const chatInput = document.getElementById('chat-input');
-    const sendButton = document.getElementById('send-button');
+// Get a reference to the "Shop Now" button
+const shopNowButton = document.getElementById('shopNowButton');
 
-    // Function to add a chat message to the chat area
-    function addChatMessage(message, isUser) {
-        const chatMessage = document.createElement('div');
-        chatMessage.className = isUser ? 'user-chat-message' : 'bot-chat-message';
-        chatMessage.textContent = message;
-        chatArea.appendChild(chatMessage);
-    }
+// Get a reference to the "productDisplayContainer"
+const productDisplayContainer = document.getElementById('productDisplayContainer');
 
-    // Function to handle sending a chat message
-    function sendChatMessage() {
-        const userMessage = chatInput.value;
-        if (userMessage) {
-            addChatMessage(userMessage, true);
-            chatInput.value = ''; // Clear the input field
-
-            // Send user's message to the server
-            fetch('/submit_chat', {
-                method: 'POST',
-                body: new URLSearchParams({ user_message: userMessage }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                const botResponse = data.bot_response;
-                addChatMessage(botResponse, false);
-            })
-            .catch(error => {
-                console.error('Error sending message:', error);
-            });
-        }
-    }
-
-    // Event listener for sending chat message
-    sendButton.addEventListener('click', sendChatMessage);
-
-    // Event listener for Enter key in chat input
-    chatInput.addEventListener('keyup', (event) => {
-        if (event.key === 'Enter') {
-            sendChatMessage();
-        }
-    });
+// Add a click event listener to the button
+shopNowButton.addEventListener('click', function() {
+  // Toggle the display property between 'none' and 'flex'
+  if (productDisplayContainer.style.display === 'none' || productDisplayContainer.style.display === '') {
+    productDisplayContainer.style.display = 'flex';
+  } else {
+    productDisplayContainer.style.display = 'none';
+  }
 });
 
-// Event listener for sending chat message
-sendChatButton.addEventListener('click', () => {
-    // Get the chat input text
-    const userInput = chatInput.value;
-    if (userInput) {
-        addChatMessage(userInput, true);
-        chatInput.value = ''; // Clear the input field
-        sendChatMessage(userInput);
-    }
-});
-
-// Event listener for Enter key in chat input
-chatInput.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-        // Get the chat input text
-        const userInput = chatInput.value;
-        if (userInput) {
-            addChatMessage(userInput, true);
-            chatInput.value = ''; // Clear the input field
-            sendChatMessage(userInput);
-        }
-    }
-});
 
 // Event listener for generate prompt button
 generatePromptButton.addEventListener('click', async () => {
@@ -93,4 +39,4 @@ document.addEventListener('DOMContentLoaded', function () {
             card.querySelector('.flip-card-inner').classList.toggle('flipped');
         });
     });
-    });
+    });    
