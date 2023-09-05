bda61e92fcb22af887adba5aba7ffb94570328d9
ankur verma <verma.ankur1729@gmail.com>
2023-09-05 17:04:23 +0530

    modified styling of all pages

--- website/static/marketingpage/scripts.js
+++ website/static/marketingpage/scripts.js
@@ -1,3 +1,42 @@
+// Get a reference to the "Regenerate Promotional Text" button
+const reloadButton = document.querySelector('.reload-button button');
+
+// Get a reference to the "User Promotional Text" ul
+const userPromotionalTextUl = document.getElementById('user-promotional-text');
+
+// Add an event listener to the "Regenerate Promotional Text" button
+reloadButton.addEventListener('click', function() {
+  // Get the promotional text from the "Promotional Text" div
+  const promotionalText = document.querySelector('.template h4').nextSibling.textContent.trim();
+
+  // Store the promotional text in an array in localStorage
+  const storedPromotionalTexts = JSON.parse(localStorage.getItem('promotionalTexts')) || [];
+  storedPromotionalTexts.push(promotionalText);
+  localStorage.setItem('promotionalTexts', JSON.stringify(storedPromotionalTexts));
+
+  // Clear the user promotional text ul
+  userPromotionalTextUl.innerHTML = '';
+
+  // Create a new list item for each promotional text and append it to the "User Promotional Text" ul
+  for (const text of storedPromotionalTexts) {
+    const listItem = document.createElement('li');
+    listItem.textContent = text;
+    userPromotionalTextUl.appendChild(listItem);
+  }
+
+  // Scroll to the bottom of the "User Promotional Text" ul to show the latest text
+  userPromotionalTextUl.scrollTop = userPromotionalTextUl.scrollHeight;
+});
+
+// Retrieve and display stored promotional texts from localStorage
+const storedPromotionalTexts = JSON.parse(localStorage.getItem('promotionalTexts')) || [];
+for (const text of storedPromotionalTexts) {
+  const listItem = document.createElement('li');
+  listItem.textContent = text;
+
+  userPromotionalTextUl.appendChild(listItem);
+}
+
 // Get a reference to the "Shop Now" button
 const shopNowButton = document.getElementById('shopNowButton');
 
@@ -40,3 +79,5 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
     });    
+
+    
