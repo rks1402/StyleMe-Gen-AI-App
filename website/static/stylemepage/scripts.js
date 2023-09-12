53392cf5014c6eda3e7e64c3994bd4d827fbf189
ankur verma <verma.ankur1729@gmail.com>
2023-09-12 22:42:50 +0530

    style change

--- website/static/stylemepage/scripts.js
+++ website/static/stylemepage/scripts.js
@@ -1,37 +1,70 @@
-// JavaScript code for adding messages
-function addBotMessage(message) {
-    const chatBox = document.getElementById('chat-box');
-    const messageElement = document.createElement('div');
-    messageElement.textContent = message;
-    messageElement.classList.add('message', 'bot-message');
-    chatBox.appendChild(messageElement);
-    // Scroll chat box to the bottom
-    chatBox.scrollTop = chatBox.scrollHeight;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    // Add a click event listener to the "Send" button
+    const sendButton = document.getElementById('send-button');
+    const userInput = document.getElementById('user-input');
+    
+    sendButton.addEventListener('click', () => {
+        const userQuestion = userInput.value;
 
-function addCustomerMessage(message) {
-    const chatBox = document.getElementById('chat-box');
-    const messageElement = document.createElement('div');
-    messageElement.textContent = message;
-    messageElement.classList.add('message', 'customer-message');
-    chatBox.appendChild(messageElement);
-    // Clear the input field
-    document.getElementById('user-input').value = '';
-    // Scroll chat box to the bottom
-    chatBox.scrollTop = chatBox.scrollHeight;
-}
+        if (!userQuestion) {
+            return; // Do nothing if the input is empty
+        }
 
-// Event listener for the send button
-document.getElementById('send-button').addEventListener('click', function() {
-    const userInput = document.getElementById('user-input').value;
-    if (userInput) {
-        addCustomerMessage(userInput);
-        // Here, you would typically call a function to process the user's input and get a bot response
-        // For now, I'll simulate a bot response after a short delay
-        setTimeout(function() {
-            addBotMessage("Here's a stylish dress recommendation for you!");
-        }, 1000);
-    }
+        // Display the user's message in the chatbox
+        addUserMessage(userQuestion);
+
+        // Clear the user input field
+        userInput.value = '';
+
+        // Make a request to the Cloud Run API with userQuestion
+        fetch('/qna', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ question: userQuestion }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            const answer = data.answer;
+
+            // Display the bot's response in the chatbox
+            addBotMessage(answer);
+        })
+        .catch(error => {
+            console.error('Error fetching answer:', error);
+        });
+    });
+    
 });
 
+// Function to add a user message to the chatbox
+function addUserMessage(message) {
+    const chatbox = document.getElementById('chat-box');
+    const userMessageElement = document.createElement('div');
+    userMessageElement.classList.add('message', 'user-message');
+    userMessageElement.textContent = message;
+    chatbox.appendChild(userMessageElement);
+}
 
+// Function to add a bot message to the chatbox
+function addBotMessage(message) {
+    const chatbox = document.getElementById('chat-box');
+    const botMessageElement = document.createElement('div');
+    botMessageElement.classList.add('message', 'bot-message');
+    botMessageElement.textContent = message;
+    chatbox.appendChild(botMessageElement);
+}
+
+function flipCard(element) {
+    element.closest('.flip-card').classList.toggle('flipped');
+}
+
+// Get the height of both containers
+const chatContainer = document.querySelector('.chat-container');
+const productContainer = document.querySelector('.product-container');
+const maxHeight = Math.max(chatContainer.clientHeight, productContainer.clientHeight);
+
+// Set both containers to have the same height
+chatContainer.style.height = `${maxHeight}px`;
+productContainer.style.height = `${maxHeight}px`;
