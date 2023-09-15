58e519da47d62d7cf6c7c7c41f97cc434c466078
2137942shubham <Shubham.Mishra2366d0@cognizant.com>
2023-09-15 10:04:01 +0530

    dynamic product recommendation

--- website/static/stylemepage/scripts.js
+++ website/static/stylemepage/scripts.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Add a click event listener to the "Send" button
     const sendButton = document.getElementById('send-button');
     const userInput = document.getElementById('user-input');
-    
+    const chatbox = document.getElementById('chat-box');
+    const productContainer = document.querySelector('.product-container');
+
+    // Initialize the conversation list
+    const conversationList = [];
+
     sendButton.addEventListener('click', () => {
         const userQuestion = userInput.value;
 
@@ -16,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Clear the user input field
         userInput.value = '';
 
-        // Make a request to the Cloud Run API with userQuestion
+        // Make a request to the Cloud Run API with userQuestion for chatbot response
         fetch('/stylemeqna', {
             method: 'POST',
             headers: {
@@ -30,15 +34,59 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Display the bot's response in the chatbox
             addBotMessage(answer);
+
+            // Add the conversation to the list
+            conversationList.push({ 'User': userQuestion, 'Fashion Advisor': answer });
+
+            // Now, make a request to fetch products based on the conversation
+            fetch('/styleme', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ conversation: conversationList }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Assuming that 'data.products' contains the list of products
+                const products = data.products.slice(0, 3);
+
+                // Clear existing products in the productContainer
+                productContainer.innerHTML = '';
+
+                // Add the new products to the productContainer
+                products.forEach(product => {
+                    const productDiv = document.createElement('div');
+                    productDiv.className = 'product';
+                    productDiv.innerHTML = `
+                        <a href="${product.product_url}">
+                            <img src="${product.image_URL}" alt="${product.product_name}">
+                            <h2>${product.product_name}</h2>
+                            <p>$${product.price}</p>
+                        </a>`;
+                    productContainer.appendChild(productDiv);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching and displaying products:', error);
+            });
         })
         .catch(error => {
             console.error('Error fetching answer:', error);
         });
     });
-    
+
+    // ... (rest of your code)
+
+    // Define addUserMessage and addBotMessage functions if not already defined
 });
 
-// Function to add a user message to the chatbox
+// Rest of your code...
+
+
+
+
+
 function addUserMessage(message) {
     const chatbox = document.getElementById('chat-box');
     const userMessageElement = document.createElement('div');
@@ -85,48 +133,3 @@ const userInput = document.getElementById("user-input");
             sendButton.click();
         }
     });
-
-
-
-    /*document.addEventListener('DOMContentLoaded', () => {
-        const chatHistory = []; // Create an array to store the conversation history
-    
-        // ... (other code)
-    
-        // Add a click event listener to the "Submit Chat" button
-        const submitButton = document.getElementById('submit-button');
-    
-        submitButton.addEventListener('click', () => {
-            // Get the user's input
-            const userInput = document.getElementById('user-input').value;
-    
-            if (!userInput) {
-                return; // Do nothing if the input is empty
-            }
-    
-            // Add the user's message to the conversation history
-            chatHistory.push({ role: 'user', message: userInput });
-    
-            // Clear the user input field
-            document.getElementById('user-input').value = '';
-    
-            // Make a request to the server with the entire conversation
-            fetch('/submit_chat', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ conversation: chatHistory }), // Send the entire conversation
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Handle the response as needed
-            })
-            .catch(error => {
-                console.error('Error submitting chat:', error);
-            });
-        });
-    
-        // ... (other code)
-    });
-    */
